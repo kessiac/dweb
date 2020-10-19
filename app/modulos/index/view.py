77628c423d9f59b893a01922b4a76043217401e4
kessiac <kessiac147@gmail.com>
2020-10-19 16:54:55 -0300

    covid

--- app/modulos/index/view.py
+++ app/modulos/index/view.py
@@ -6,6 +6,34 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def homepage():
-  exames = Exames.select(Exames.status, fn.COUNT(Exames.status)).group_by(Exames.status)
+  exames = Exames.select(Exames.status, fn.COUNT(Exames.status).alias('ct')).group_by(Exames.status)
+  boletim = {
+    0: {
+      "label": "Em analise",
+      "count": 0
+      },
+    1: {
+      "label": "Confirmados",
+      "count": 0
+      },
+    2: {
+      "label": "Descartados",
+      "count": 0
+      },
+    3: {
+      "label": "Obitos",
+      "count": 0
+      },
+    4: {
+      "label": "Internados",
+      "count": 0
+      },
+    5: {
+      "label": "Recuperados",
+      "count": 0
+      },
+  }
+  for c in exames:
+    boletim[c.status]['count'] = c.ct
   print(exames.scalar())
-  return render_template('index.html', exames=exames)
+  return render_template('index.html', boletim=boletim)
