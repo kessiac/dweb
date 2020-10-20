ca234c89a04ab4b9df0971d29d264ecaa3e33b17
Davi_sesion <davisesion1@gmail.com>
2020-10-20 01:16:56 +0000

    update

--- app/modulos/admin/view.py
+++ app/modulos/admin/view.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from .form import LoginForm
 
 admin = Blueprint("admin", __name__)
 
-@admin.route("/admin")
+@admin.route("/admin", methods=['GET','POST'])
 def adminpage():
-  return render_template('admin.html')
+  form = LoginForm(request.form)
+  if form.validate_on_submit():
+    if form.user.data == 'admin' and form.senha.data == 'admin':
+      return redirect(url_for('cons.consultaspage'))
+    else:
+      return render_template('admin.html', form=form, error = "Ususário ou senha incorretos")
+  return render_template('admin.html', form=form)
